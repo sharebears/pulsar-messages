a47b0c94575f80db0a32302aa20f754c116658e6
lights <lights@tutanota.de>
2018-10-15 22:01:34 -0400

    add initial branch db migration

---
+++ versions/7cda57a5e25b_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 7cda57a5e25b
+Revises: 
+Create Date: 2018-10-15 22:00:54.588159
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7cda57a5e25b'
+down_revision = None
+branch_labels = ('messages',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
