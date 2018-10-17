326909f66843c65501f28c1eebf5e3be1297ec49
lights <lights@tutanota.de>
2018-10-17 13:09:56 -0400

    update models per snaple

--- messages/models.py
+++ messages/models.py
@@ -1,5 +1,6 @@
 from core import db
-from sqlalchemy import func
+from sqlalchemy import func, select, and_, exists
+from sqlalchemy.ext.hybrid import hybrid_property
 from core.mixins import SinglePKMixin, MultiPKMixin
 
 
@@ -9,20 +10,33 @@ class PMConversation(db.Model, SinglePKMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     topic = db.Column(db.String(128), nullable=False)
+    last_updated_time = db.Column(
+        db.DateTime(timezone=True), nullable=False, server_default=func.now(), index=True)
 
 
-class PMConversationOfUser(db.Model, MultiPKMixin):
-    __tablename__ = 'pm_conversations_users'
-    __cache_key__ = 'pm_convesations_users_{conv_id}_{user_id}'
+class PMConversationState(db.Model, MultiPKMixin):
+    __tablename__ = 'pm_conversations_state'
+    __cache_key__ = 'pm_convesations_state_{conv_id}_{user_id}'
 
     conv_id = db.Column(db.Integer, db.ForeignKey('pm_conversations.id'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    in_inbox = db.Column(db.Boolean, nullable=False, index=True)
-    in_sentbox = db.Column(db.Boolean, nullable=False, index=True)
-    time = db.Column(
-        db.DateTime(timezone=True), nullable=False, server_default=func.now(), index=True)
     read = db.Column(db.Boolean, nullable=False, server_default='f')
     sticky = db.Column(db.Boolean, nullable=False, server_default='f', index=True)
+
+    @hybrid_property
+    def in_inbox(cls):
+        return select(exists().where(and_(
+            PMMessages.conv_id == cls.conv_id,
+            PMMessages.user_id == cls.user_id
+            ))).as_scalar()
+        pass
+
+    @hybrid_property
+    def in_sentbox(cls):
+        return select(~exists().where(and_(
+            PMMessages.conv_id == cls.conv_id,
+            PMMessages.user_id == cls.user_id
+            ))).as_scalar()
 
 
 class PMMessages(db.Model, SinglePKMixin):
